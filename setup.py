f2f1c7c021fb1b78c7fc45f158748e54f36a1edc
Pyth-n <19356494+Pyth-n@users.noreply.github.com>
2020-06-08 20:12:11 -0700

    Add black dependency

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ from lazy_ac_wishlist.version import __version__
 
 exec(open('lazy_ac_wishlist/version.py').read())
 setup(name='lazyacwishlist', version=__version__, packages = find_packages(),
-    install_requires=['selenium'])
+    install_requires=['selenium, black'])
